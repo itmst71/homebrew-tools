a8e3e328172eddefd7719e30cb8ed39a954c4ba2
itmst71 <itmst71@gmail.com>
2017-02-13 10:06:25 +0900

    Release cda v1.0.0

---
+++ cda.rb
@@ -0,0 +1,21 @@
+require "formula"
+
+class Cda < Formula
+  desc "cd with an alias name"
+  homepage "https://github.com/itmst71/cda"
+  url "https://github.com/itmst71/cda.git", :tag => "v1.0.0"
+  head "https://github.com/itmst71/cda.git", :branch => "master"
+  version "1.0.0"
+
+  def install
+    prefix.install Dir['*']
+  end
+
+  def caveats
+    msg = <<-EOF.undent
+    Add the following line to your ~/.bashrc or ~/.zshrc file (and remember to source the file to update your current session):
+
+      [[ -f "$(brew --prefix cda)/cda.sh" ]] && . "$(brew --prefix cda)/cda.sh"
+    EOF
+  end
+end
